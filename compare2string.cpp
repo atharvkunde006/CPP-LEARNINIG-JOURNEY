e8805f6fedd58bd5ac8f3879522459321966f635
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-25 22:47:08 +0530

    Add files via upload

---
+++ compare2string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    string a="Atharv",b="Kunde";
+    if(a==b){
+        cout<<"Both are equal";
+    }
+    else{
+        cout<<"Not equal";
+    }
+}
---
+++ reverce.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    string str="Atharv";
+    reverse(str.begin(),str.end());
+     cout<<str;
+
+}
