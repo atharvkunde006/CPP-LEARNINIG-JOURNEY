2b2e8704851959ff0d013f385b7fd264f409ead8
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-27 21:21:53 +0530

    Add files via upload

---
+++ printnumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int arr[]={1,3,4,5,6,7};
+cout<<arr[0]<<endl;
+cout<<arr[1]<<endl;
+cout<<arr[2]<<endl;
+cout<<arr[3]<<endl;
+cout<<arr[4]<<endl;
+cout<<arr[5]<<endl;
+}
