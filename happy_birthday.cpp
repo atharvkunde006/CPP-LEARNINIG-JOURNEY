ae18b217babca7e6eaf8def8253559a99bae01c1
Atharv Kunde <atharvkunde006@gmail.com>
2026-03-30 19:44:46 +0530

    Add files via upload

---
+++ happy_birthday.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void atharv()
+{
+    cout << "I WISHING YOU:" << endl;
+    cout << "Happy birthday to atharv Baby:" << endl;
+}
+int main()
+{
+    atharv();
+    atharv();
+}
